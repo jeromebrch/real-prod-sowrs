f7d913a3face8cd5d06204734589d9e1e77ba2b3
Jérôme BRIANCHON <jerome.brch@gmail.com>
2021-04-06 17:28:03 +0200

    récupération erreurs

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -50,7 +50,6 @@ class MessageController extends AbstractController
             'category' => $category,
             'nonlus' => $messageState,
             'nonlu' => $nonlu
-
         ]);
     }
 
