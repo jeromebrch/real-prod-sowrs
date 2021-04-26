b062b9672e30ae6df4790448d71398e9a72510a0
Jérôme BRIANCHON <jerome.brch@gmail.com>
2021-04-26 09:29:08 +0200

    commit try resolve SSl bug

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
+    FOS\MessageBundle\FOSMessageBundle::class => ['all' => true],
 ];
